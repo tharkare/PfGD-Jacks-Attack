42f0507a431ede999827af45fa377be1eacde70a
Tanvi H <tharkare@andrew.cmu.edu>
2020-05-04 23:34:05 -0400

    Movement to Character
    New Sprites put into game play area. The main character moves, and the camera moves along with it

---
+++ Assets/Scenes/MovePlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update(){
+        //Move Player Right
+        if(Input.GetKey(KeyCode.LeftArrow)){
+            this.transform.Translate (new Vector3 (-3f, 0f, 0f) * Time.deltaTime,Space.World);
+		}    
+        else if(Input.GetKey(KeyCode.RightArrow)){
+            this.transform.Translate (new Vector3 (3f, 0f, 0f) * Time.deltaTime,Space.World);  
+		}
+        else if(Input.GetKey(KeyCode.UpArrow)){
+            this.transform.Translate (new Vector3 (0f, 8f, 0f) * Time.deltaTime,Space.World);
+		}
+    }
+}
---
+++ Assets/Scripts/Controls.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float movespeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);  
+		}
+        if (Input.GetKey(KeyCode.RightArrow)){
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);  
+		}
+        
+    }
+}
