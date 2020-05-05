edcd81c784881d74bcccbd2a3b6649b0bb4b49bd
Tanvi H <tharkare@andrew.cmu.edu>
2020-05-05 00:26:17 -0400

    Jump Icon
    The player can now move the character left and right using the buttons provided on screen

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -6,12 +6,13 @@ public class Controls : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float movespeed;
+    public bool moveright;
+    public bool moveleft;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        
+        rb = GetComponent<Rigidbody2D>();     
     }
 
     // Update is called once per frame
@@ -22,6 +23,13 @@ public class Controls : MonoBehaviour
 		}
         if (Input.GetKey(KeyCode.RightArrow)){
             rb.velocity = new Vector2(movespeed, rb.velocity.y);  
+		}
+
+        if (moveright){
+            rb.velocity = new Vector2(movespeed, rb.velocity.y);
+		}
+        if (moveleft){
+            rb.velocity = new Vector2(-movespeed, rb.velocity.y);  
 		}
         
     }
