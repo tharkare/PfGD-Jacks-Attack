c998fc24a03a1602220c5cf15acb88f88427d5af
Tanvi H <tharkare@andrew.cmu.edu>
2020-05-05 01:02:16 -0400

    Jump Button
    The player now has the option to use a jump button to move the character. Still need tweaking on ground checking component

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -8,6 +8,16 @@ public class Controls : MonoBehaviour
     public float movespeed;
     public bool moveright;
     public bool moveleft;
+    public bool jump;
+    public float jumpheight;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    private bool onGround;
+
+    void FixedUpdate(){
+        onGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+	}
 
     // Start is called before the first frame update
     void Start()
@@ -24,7 +34,13 @@ public class Controls : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow)){
             rb.velocity = new Vector2(movespeed, rb.velocity.y);  
 		}
-
+        if (Input.GetKey(KeyCode.Space)){
+            rb.velocity = new Vector2(rb.velocity.x, jumpheight);  
+		}
+        if (jump){
+            rb.velocity = new Vector2(rb.velocity.x, jumpheight);
+            jump = false;
+		} 
         if (moveright){
             rb.velocity = new Vector2(movespeed, rb.velocity.y);
 		}
--- Assets/Scripts/Touch.cs
+++ Assets/Scripts/Touch.cs
@@ -29,6 +29,10 @@ public class Touch : MonoBehaviour
         player.moveright = false;
 	}
 
+    public void Jump(){
+     player.jump = true;
+	}
+
     // Update is called once per frame
     void Update()
     {
